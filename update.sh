86e815b8dcbfa2e3eae8055454cd63283c982027
Tianon Gravi <admwiggin@gmail.com>
2017-04-24 07:25:38 -0700

    Modify update.sh to skip releases that are missing release artifacts (like v0.14.27 currently is)

--- update.sh
+++ update.sh
@@ -19,7 +19,25 @@ for version in "${versions[@]}"; do
 	if [ "$rcVersion" != "$version" ]; then
 		rcGrepV=
 	fi
-	fullVersion="$(echo "$tags" | grep "^v$rcVersion[.]" | grep $rcGrepV -E -- '-alpha|-beta|-rc' | head -n1)"
+
+	IFS=$'\n'; fullVersions=( $(echo "$tags" | grep "^v$rcVersion[.]" | grep $rcGrepV -E -- '-alpha|-beta|-rc') ); unset IFS
+	case "$version" in
+		0.10|0.11|0.12|0.13) fullVersion="${fullVersions[0]}" ;;
+		*)
+			fullVersion=
+			for possible in "${fullVersions[@]}"; do
+				# make sure the version we include actually has release artifacts
+				tarball="https://github.com/syncthing/syncthing/releases/download/${possible}/sha256sum.txt.asc"
+				# "wget --spider" on a github release artifact doesn't work properly (sometimes?), so we just download the full sha256sum file instead
+				#tarball="https://github.com/syncthing/syncthing/releases/download/${possible}/syncthing-linux-amd64-${possible}.tar.gz"
+				if wget --quiet -O /dev/null "$tarball" &> /dev/null; then
+					fullVersion="$possible"
+					break
+				fi
+				echo >&2 "warning: $possible appears to be missing release artifacts; ignoring"
+			done
+			;;
+	esac
 	if [ -z "$fullVersion" ]; then
 		echo >&2 "warning: cannot find full version for $version"
 		continue
